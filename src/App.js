95dc5c1f350fdab0d5f96c8680184c7c4ed464f0
memolop10 <inglopezvargas@gmail.com>
2021-02-28 16:32:11 -0600

    routes complete

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  NavLink
+} from "react-router-dom";
+import Inicio from './components/Inicio';
+import Contacto from './components/Contacto';
+import Nosotros from './components/Nosotros';
+
+function App() {
+  return (
+    <Router>
+      <div className="container mt-5">
+        <div className="btn-group">
+          <Link to="/" className="btn btn-dark">Inicio</Link>
+          <Link to="/contacto" className="btn btn-dark">Contacto</Link>
+          <NavLink to="/nosotros" className="btn btn-dark" activeStyle={{
+            color:"red"
+          }}>Nosotros</NavLink>
+        </div>
+        <hr/>
+        <Switch>
+          <Route path="/contacto">
+            <Contacto/>
+          </Route>
+          <Route path="/nosotros">
+            <Nosotros/>
+          </Route>
+          <Route path="/" exact>
+            <Inicio/>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
