48c53219e777b48829c181641ab4b5efbe768d86
memolop10 <inglopezvargas@gmail.com>
2021-03-02 09:23:21 -0600

    rutas dinamicas

--- src/components/Nosotros.jsx
+++ src/components/Nosotros.jsx
@@ -1,9 +1,32 @@
 import React from 'react'
 
 const Nosotros = () => {
+
+    const [equipo,setEquipo] = React.useState([])
+      
+    React.useEffect(() => {
+        obtenerDatos()
+    },[]);
+
+    const obtenerDatos = async() => {
+        const data = await fetch('https://age-of-empires-2-api.herokuapp.com/api/v1/civilizations')
+        const users = await data.json()
+
+        setEquipo(users.civilizations)
+    }
+
     return (
         <div>
-            <h1>Nosotros</h1> 
+            <h1>Nosotros</h1>
+            <ul>
+                {
+                    equipo.map((item) => (
+                        <li key={item.id}>
+                            {item.name} - {item.expansion   }
+                        </li>
+                    ))
+                }
+            </ul> 
         </div>
     )
 }
